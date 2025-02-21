57b6224b6b66c352149ece7765f00b789ece1fde
Aidan Do <aidand@canva.com>
2025-02-22 10:56:40 +1100

    .

--- COMMANDS.md
+++ COMMANDS.md
@@ -7,4 +7,13 @@ conda activate ./env
 pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118
 
 python hello.py
+
+
+
+# For CUDA easy intro into
+# https://developer.nvidia.com/blog/even-easier-introduction-cuda
+wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/x86_64/cuda-keyring_1.0-1_all.deb
+sudo dpkg -i cuda-keyring_1.0-1_all.deb
+sudo apt-get update
+sudo apt-get -y install cuda
 ```
--- hello.py
+++ hello.py
@@ -1,6 +1,25 @@
 import torch
 import torch.nn.functional as F
 import math
+import lltm_cpp
+
+class LLTMFunction(torch.autograd.Function):
+    @staticmethod
+    def forward(ctx, input, weights, bias, old_h, old_cell):
+        outputs = lltm_cpp.forward(input, weights, bias, old_h, old_cell)
+        new_h, new_cell = outputs[:2]
+        variables = outputs[1:] + [weights]
+        ctx.save_for_backward(*variables)
+
+        return new_h, new_cell
+
+    @staticmethod
+    def backward(ctx, grad_h, grad_cell):
+        outputs = lltm_cpp.backward(
+            grad_h.contiguous(), grad_cell.contiguous(), *ctx.saved_tensors)
+        d_old_h, d_input, d_weights, d_bias, d_old_cell = outputs
+        return d_input, d_weights, d_bias, d_old_h, d_old_cell
+
 class LLTM(torch.nn.Module):
     def __init__(self, input_features, state_size):
         super(LLTM, self).__init__()
@@ -19,24 +38,27 @@ class LLTM(torch.nn.Module):
             weight.data.uniform_(-stdv, +stdv)
 
     def forward(self, input, state):
-        old_h, old_cell = state
-        X = torch.cat([old_h, input], dim=1)
+        return LLTMFunction.apply(input, self.weights, self.bias, *state)
 
-        # Compute the input, output and candidate cell gates with one MM.
-        gate_weights = F.linear(X, self.weights, self.bias)
-        # Split the combined gate weight matrix into its components.
-        gates = gate_weights.chunk(3, dim=1)
+    # def forward(self, input, state):
+    #     old_h, old_cell = state
+    #     X = torch.cat([old_h, input], dim=1)
 
-        input_gate = torch.sigmoid(gates[0])
-        output_gate = torch.sigmoid(gates[1])
-        # # Here we use an ELU instead of the usual tanh.
-        candidate_cell = F.elu(gates[2])
+    #     # Compute the input, output and candidate cell gates with one MM.
+    #     gate_weights = F.linear(X, self.weights, self.bias)
+    #     # Split the combined gate weight matrix into its components.
+    #     gates = gate_weights.chunk(3, dim=1)
 
-        # Compute the new cell state.
-        new_cell = old_cell + candidate_cell * input_gate
-        # Compute the new hidden state and output.
-        new_h = torch.tanh(new_cell) * output_gate
-        return new_h, new_cell
+    #     input_gate = torch.sigmoid(gates[0])
+    #     output_gate = torch.sigmoid(gates[1])
+    #     # # Here we use an ELU instead of the usual tanh.
+    #     candidate_cell = F.elu(gates[2])
+
+    #     # Compute the new cell state.
+    #     new_cell = old_cell + candidate_cell * input_gate
+    #     # Compute the new hidden state and output.
+    #     new_h = torch.tanh(new_cell) * output_gate
+    #     return new_h, new_cell
 
 def test_hello():
     batch_size = 3
@@ -103,3 +125,33 @@ def test_lltm():
 
 if __name__ == "__main__":
     test_hello()
+
+    import time
+
+    import torch
+
+    assert torch.cuda.is_available()
+    cuda_device = torch.device("cuda")  # device object representing GPU
+
+    batch_size = 16
+    input_features = 32
+    state_size = 128
+
+    X = torch.randn(batch_size, input_features)
+    h = torch.randn(batch_size, state_size)
+    C = torch.randn(batch_size, state_size)
+
+    rnn = LLTM(input_features, state_size)
+
+    forward = 0
+    backward = 0
+    for _ in range(100000):
+        start = time.time()
+        new_h, new_C = rnn(X, (h, C))
+        forward += time.time() - start
+
+        start = time.time()
+        (new_h.sum() + new_C.sum()).backward()
+        backward += time.time() - start
+
+    print('Forward: {:.3f} s | Backward {:.3f} s'.format(forward, backward))
