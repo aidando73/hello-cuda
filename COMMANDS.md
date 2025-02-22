7ae333749324cb75cfd2a272211d8be3ad90da8e
Aidan Do <aidand@canva.com>
2025-02-22 11:23:26 +1100

    .

--- COMMANDS.md
+++ COMMANDS.md
@@ -17,4 +17,10 @@ wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/x86_64/
 sudo dpkg -i cuda-keyring_1.0-1_all.deb
 sudo apt-get update
 sudo apt-get -y install cuda
+
+export PATH=/usr/local/cuda/bin:$PATH
+export LD_LIBRARY_PATH=/usr/local/cuda/lib64:$LD_LIBRARY_PATH
+
+sudo apt update
+sudo apt install clang build-essential
 ```
--- hello.py
+++ hello.py
@@ -37,28 +37,28 @@ class LLTM(torch.nn.Module):
         for weight in self.parameters():
             weight.data.uniform_(-stdv, +stdv)
 
-    def forward(self, input, state):
-        return LLTMFunction.apply(input, self.weights, self.bias, *state)
-
     # def forward(self, input, state):
-    #     old_h, old_cell = state
-    #     X = torch.cat([old_h, input], dim=1)
+    #     return LLTMFunction.apply(input, self.weights, self.bias, *state)
 
-    #     # Compute the input, output and candidate cell gates with one MM.
-    #     gate_weights = F.linear(X, self.weights, self.bias)
-    #     # Split the combined gate weight matrix into its components.
-    #     gates = gate_weights.chunk(3, dim=1)
+    def forward(self, input, state):
+        old_h, old_cell = state
+        X = torch.cat([old_h, input], dim=1)
 
-    #     input_gate = torch.sigmoid(gates[0])
-    #     output_gate = torch.sigmoid(gates[1])
-    #     # # Here we use an ELU instead of the usual tanh.
-    #     candidate_cell = F.elu(gates[2])
+        # Compute the input, output and candidate cell gates with one MM.
+        gate_weights = F.linear(X, self.weights, self.bias)
+        # Split the combined gate weight matrix into its components.
+        gates = gate_weights.chunk(3, dim=1)
 
-    #     # Compute the new cell state.
-    #     new_cell = old_cell + candidate_cell * input_gate
-    #     # Compute the new hidden state and output.
-    #     new_h = torch.tanh(new_cell) * output_gate
-    #     return new_h, new_cell
+        input_gate = torch.sigmoid(gates[0])
+        output_gate = torch.sigmoid(gates[1])
+        # # Here we use an ELU instead of the usual tanh.
+        candidate_cell = F.elu(gates[2])
+
+        # Compute the new cell state.
+        new_cell = old_cell + candidate_cell * input_gate
+        # Compute the new hidden state and output.
+        new_h = torch.tanh(new_cell) * output_gate
+        return new_h, new_cell
 
 def test_hello():
     batch_size = 3
@@ -127,7 +127,6 @@ if __name__ == "__main__":
     test_hello()
 
     import time
-
     import torch
 
     assert torch.cuda.is_available()
@@ -137,21 +136,24 @@ if __name__ == "__main__":
     input_features = 32
     state_size = 128
 
-    X = torch.randn(batch_size, input_features)
-    h = torch.randn(batch_size, state_size)
-    C = torch.randn(batch_size, state_size)
+    # Note the device=cuda_device arguments here
+    X = torch.randn(batch_size, input_features, device=cuda_device)
+    h = torch.randn(batch_size, state_size, device=cuda_device)
+    C = torch.randn(batch_size, state_size, device=cuda_device)
 
-    rnn = LLTM(input_features, state_size)
+    rnn = LLTM(input_features, state_size).to(cuda_device)
 
     forward = 0
     backward = 0
     for _ in range(100000):
         start = time.time()
         new_h, new_C = rnn(X, (h, C))
+        torch.cuda.synchronize()
         forward += time.time() - start
 
         start = time.time()
         (new_h.sum() + new_C.sum()).backward()
+        torch.cuda.synchronize()
         backward += time.time() - start
 
-    print('Forward: {:.3f} s | Backward {:.3f} s'.format(forward, backward))
+    print('Forward: {:.3f} us | Backward {:.3f} us'.format(forward * 1e6/1e5, backward * 1e6/1e5))
